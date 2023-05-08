8859ffc39015a1b895e060a63d0c3dbef8999f13
hoductrung1211 <hoductrung2604@gmail.com>
2023-05-08 21:58:08 +0700

    Fix user

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,30 +4,20 @@ export default function UserList({
     return (
         <>
             <h3 className="text-2xl text-center font-extrabold text-yellow-200">Danh sách trúng thưởng</h3>
-            <section className="mt-4 w-180 px-3 divide-y-2 rounded-md max-h-100">
+            <section className="mt-4 w-96 md-e-140 px-3 divide-y-2 rounded-md max-h-100 bg-white">
             {
                 users.map(user => 
-                    <div className=" grid grid-cols-4 gap-y-2 bg-white">
-                        <Cell text={user.phone} />
-                        <Cell text={user.fullName} />
-                        <Cell text={user.gift} />
-                        <Cell text={user.spinnedDate.slice(0, 19)} />
+                    <div className="w-full h-12 px-2 items-center grid grid-cols-4">
+                        <span className="col-span-1">{user.phone}</span>
+                        <span className="col-span-1">{user.fullName}</span>
+                        <span className="col-span-1">{user.gift}</span>
+                        <span className="col-span-1">{user.spinnedDate.slice(0,10)}</span>
                     </div>    
                 )
-                
             }
-            
             </section>
         </>
     )
 }
 
-function Cell({
-    text
-}) {
-    return (
-        <div className="col-span-1 h-12 px-2 flex items-center">
-            {text}
-        </div>
-    )
-}
+ 
