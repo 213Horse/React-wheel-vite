1f879ac43a30e57e29f51578f21e4524cab47272
hoductrung1211 <hoductrung2604@gmail.com>
2023-05-10 10:56:39 +0700

    Fix email regex

--- src/components/form/UserInfoForm.jsx
+++ src/components/form/UserInfoForm.jsx
@@ -47,7 +47,7 @@ export default function UserInfoForm({
 
     function validateInfo() {
         const fullNameRegex = /^[\p{L}\p{M}'\-]+(?:\s[\p{L}\p{M}'\-]+)*$/u;
-        const emailRegex = /^\S+@\S+\.\S+$/;        
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;        
 
         let fullNameError = null;
         let phoneNumberError = null;
