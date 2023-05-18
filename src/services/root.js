594c563a60a76a4f9ac5a8ae791a6eea59b3d3bb
tanpnse2k2 <phannhattan201447@gmail.com>
2023-05-18 11:27:32 +0700

    change root to api-vongquay

--- src/services/root.js
+++ src/services/root.js
@@ -1,6 +1,6 @@
-const root = 'https://localhost:7298';
+//const root = 'https://localhost:7298';
 export const rootBsmart = 'https://bsmart.edu.vn';
 //export const rootBsmart = 'https://dev.bsmart.edu.vn';
 //export const rootBsmart = 'https://localhost:44312';
-// const root = 'https://api-vongquay.bsmart.edu.vn'; 
+const root = 'https://api-vongquay.bsmart.edu.vn'; 
 export default root;
